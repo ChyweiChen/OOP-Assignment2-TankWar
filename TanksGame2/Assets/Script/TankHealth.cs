4c4279ea163f2aa1f28813ade97c75278f3fe071
ChyweiChen <d16123951@mydit.ie>
2018-04-03 14:19:00 +0100

    Update by Yuwei Chen

---
+++ TanksGame2/Assets/Script/TankHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour {
+
+    public int hp = 100;
+    public GameObject tanExplosion;
+    public AudioClip tankExplosionAudio;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage()
+    {
+        if (hp <= 0) return;
+        hp -= Random.Range(8,15);
+        if (hp <= 0)
+        {
+            AudioSource.PlayClipAtPoint(tankExplosionAudio, transform.position);
+            GameObject.Instantiate(tanExplosion, transform.position + Vector3.up, transform.rotation);
+            GameObject.Destroy(this.gameObject);
+        }
+
+    }
+}
