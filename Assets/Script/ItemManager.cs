120d277c9e1c51d918ce19fce4426740863b9f88
ChyweiChen <d16123951@mydit.ie>
2018-04-24 17:20:38 +0100

    hhhhh

---
+++ Assets/Script/ItemManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ItemManager : MonoBehaviour {
+
+    public GameObject Add_Health;
+    public GameObject Add_Damage;
+    public GameObject Add_Barrel;
+    //public GameObject Add_Collider;
+    public int ItemType =0 ;
+
+    public GameObject[] ItemPos;
+    //Boolean Health, Damage, Barrel = false;
+    public int No_Item = 0;
+    System.DateTime currentTime = new System.DateTime();
+    public int Item, Item_Pos, Ran_Item, Ran_Item_Pos = 0;
+    
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Create_Item",1, 2);
+
+    }
+
+    // Update is called once per frame
+
+    
+    int No_Item_Control()
+    {
+        if (No_Item > 0)
+        {
+            No_Item --;
+        }
+        return No_Item;
+    }
+    void Create_Item()
+    {
+        System.Random ran = new System.Random();
+
+        do
+        {
+            Ran_Item = ran.Next(0, 3);
+            
+        } while (Item == Ran_Item);
+        Item = Ran_Item;
+        do
+        {
+            Ran_Item_Pos = ran.Next(0, 6);
+        }
+        while (Item_Pos == Ran_Item_Pos);
+        Item_Pos = Ran_Item_Pos;
+        
+
+        if (No_Item < 2)
+        {
+            switch (Item)
+            {
+                case 0:
+                    Instantiate(Add_Health, ItemPos[Item_Pos].transform.position, ItemPos[Item_Pos].transform.rotation);
+                    No_Item++;
+                    break;
+                case 1:
+                    Instantiate(Add_Damage, ItemPos[Item_Pos].transform.position, ItemPos[Item_Pos].transform.rotation);
+                    No_Item++;
+                    break;
+                case 2:
+                    Instantiate(Add_Barrel, ItemPos[Item_Pos].transform.position, ItemPos[Item_Pos].transform.rotation);
+                    No_Item++;
+                    break;
+            }
+        }
+    }
+    public void OnTriggerEnter(Collider collider)
+    {
+        GameObject.Destroy(this.gameObject);
+
+        if (collider.tag == "tank")  //when buff touch tank
+        {
+
+            if (ItemType == 0)
+            {
+                collider.SendMessage("Add_Health");
+                GameObject.Find("ItemManager").SendMessage("No_Item_Control");
+                GameObject.Find("AddHealthAudio").SendMessage("PlayAudio");
+            }
+
+            if (ItemType == 1)
+            {
+                
+                collider.SendMessage("Add_Damage");
+                GameObject.Find("ItemManager").SendMessage("No_Item_Control");
+                GameObject.Find("AddDamageAudio").SendMessage("PlayAudio");
+            }
+
+            if (ItemType == 2)
+            {
+               
+                collider.SendMessage("Add_Speed");
+                GameObject.Find("ItemManager").SendMessage("No_Item_Control");
+                GameObject.Find("AddSpeedAudio").SendMessage("PlayAudio");
+            }
+        }
+
+            
+    }
+    void Update()
+    {
+    }
+
+}
+
---
+++ Assets/Script/Shell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour {
+
+    public GameObject shellExplosionPrefab;
+
+    public AudioClip shellExplosionAudio;
+
+
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        AudioSource.PlayClipAtPoint(shellExplosionAudio, transform.position);
+        GameObject.Instantiate(shellExplosionPrefab, transform.position, transform.rotation);
+        GameObject.Destroy(this.gameObject);
+
+        if (collider.tag == "tank")  //when shell touch tank
+        {
+            collider.SendMessage("TakeDamage");
+        }
+    }
+}
