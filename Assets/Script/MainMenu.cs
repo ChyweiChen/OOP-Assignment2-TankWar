5e9c14cb93f2f8945bd726bc095f0c0e2b6bf3c8
ChyweiChen <d16123951@mydit.ie>
2018-04-24 22:10:14 +0100

    Code improve and some game component change

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+    private void Start()
+    {
+        AudioSource starMusic = GetComponent<AudioSource>();
+        starMusic.Play();
+        
+    }
+    public void PVP()
+    {
+        SceneManager.LoadScene("Mian");
+    }
+    public void PVE()
+    {
+        SceneManager.LoadScene("Mian");
+    }
+
+    public void Quit()
+    {
+       //Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/TankMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public float angularSpeed = 30;
+    public int number = 1; //judge a player by different number 
+    private new Rigidbody rigidbody;
+
+    public AudioClip idleAudio;
+    public AudioClip drivingAudio;
+
+    public AudioSource audio;
+    // Use this for initialization
+
+    void Start() {
+        rigidbody = this.GetComponent<Rigidbody>();
+        audio = this.GetComponent<AudioSource>();
+    }
+
+    float Add_Speed()
+    {
+        speed += 5;
+        Invoke("Reduce_Speed", 10);
+        return speed;
+    }
+    float Reduce_Speed()
+    {
+        speed -= 5;
+        return speed;
+    }
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("Verticalplayer"+number);
+        rigidbody.velocity = transform.forward * v * speed; //Fore-and-aft movement speed
+
+        float h = Input.GetAxis("player" + number + "Horizontal");
+        rigidbody.angularVelocity = transform.up * h * angularSpeed; //rotational speed 
+
+        //when tank is runing
+        if (Mathf.Abs(h) > 0.1 || Mathf.Abs(v) > 0.1)
+        {
+            audio.clip = drivingAudio;
+            audio.volume = 0.2f;
+            if(audio.isPlaying == false)
+            audio.Play();
+        }
+        else {
+            audio.clip = idleAudio;
+            audio.volume = 0.2f;
+            if (audio.isPlaying == false)
+                audio.Play();
+        }
+    }
+}
